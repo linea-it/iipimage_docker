951537bee23f77e2127608f4edb7e441ad7b310c
glaubervila <glauber.vila.verde@gmail.com>
2021-01-19 15:44:36 -0300

    Added healthcheck script that indicates to the container manager the health of the container. Logrotate has been added to manage the logs generated by the iipserver.

--- iipserver_astro/start_fcgi.sh
+++ iipserver_astro/start_fcgi.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Start Cron service
+service cron start
+service cron status
+
+# Start FCGI server
+echo "Starting FCGI IIpsrv"
 spawn-fcgi -f src/iipsrv.fcgi -p 9000
-touch /tmp/iipsrv.log
-tail -f /tmp/iipsrv.log
+
+# Create and tail log file
+touch $LOGFILE
+tail -f $LOGFILE
